c206382fb647e98a2ee7025c5c54f548f91b8f2c
Shubhendu Vaid <shubhendu.vaid@bt.com>
2025-04-01 18:37:15 +0100

    002

--- shubh/01_04_25/index.js
+++ shubh/01_04_25/index.js
@@ -45,8 +45,56 @@ function palindrome(str) {
 }
 
 //O(n)
+// function reverseStr(str) {
+//   return str.split("").reverse().join("");
+// }
+
 function reverseStr(str) {
-  return str.split("").reverse().join("");
+  let temp = "";
+  for (let index = str.length - 1; index >= 0; index--) {
+    temp += str.charAt(index);
+  }
+  return temp;
+}
+
+//
+function fizzBuzz(num) {
+  let str = "";
+  for (let index = 1; index <= num; index++) {
+    let isFizz = index % 3 === 0;
+    let isBuzz = index % 5 === 0;
+    let isFizzBuzz = index % 3 === 0 && index % 5 === 0;
+
+    if (isFizzBuzz) {
+      str += "FizzBuzz, ";
+    } else if (isFizz) {
+      str += "Fizz, ";
+    } else if (isBuzz) {
+      str += "Buzz, ";
+    } else {
+      str += index + ", ";
+    }
+  }
+  return str;
+}
+
+//
+function sumOfTwoNumbers(array, target) {
+  let pair = [];
+  let map = {};
+  array.map((elem, index) => {
+    map[elem] = index;
+  });
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    let noToFind = target - element;
+    if (map[noToFind]) {
+      pair.push(index);
+      pair.push(map[noToFind]);
+      break;
+    }
+  }
+  return pair;
 }
 
 // Call sum
@@ -59,3 +107,7 @@ console.log(palindrome("Yo, Banana Boy"));
 console.log(palindrome("No lemon, no melon"));
 // Call string reversal
 console.log(reverseStr("Hello there 1234 !"));
+// Call FizzBuzz
+console.log(fizzBuzz(15));
+// Call Sumof2Nos
+console.log(sumOfTwoNumbers([2, 7, 11, 15], 9));
